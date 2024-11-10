1e5fc5a4ff94cff8d597a5e9a3ea4845a1cab945
Yaba Ernesto <yabaernesto@gmail.com>
2024-11-10 16:36:18 +0100

    ♻️ Refatorando componentes
    refatorando componentes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,12 @@
 export function Footer() {
   return (
-    <div className="px-20 pt-10 pb-8">
-      <footer className="flex justify-between items-center text-gray-500 text-base font-medium">
-        <p>Travelgram © 2024</p>
+    <footer className="px-20 pt-10 pb-8 flex justify-between items-center text-gray-500 text-base font-medium">
+      <p>Travelgram © 2024</p>
 
-        <div className="flex justify-between gap-2">
-          <p>Termos de uso</p>
-          <p>Política de privacidade</p>
-        </div>
-      </footer>
-    </div>
+      <div className="flex justify-between gap-2">
+        <p>Termos de uso</p>
+        <p>Política de privacidade</p>
+      </div>
+    </footer>
   )
 }
--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -2,56 +2,54 @@ import { Avatar } from './Image'
 
 export function Gallery() {
   return (
-    <div className="px-20 pt-5">
-      <div className="grid grid-cols-4 gap-4">
-        <Avatar
-          url="https://estamosjuntos.co.ao/wp-content/uploads/2024/06/Luanda-2.jpg"
-          alt="..."
-          className="h-[250px] w-96 object-cover rounded-md"
-        />
+    <div className="px-20 pt-5 grid grid-cols-4 gap-4">
+      <Avatar
+        url="https://estamosjuntos.co.ao/wp-content/uploads/2024/06/Luanda-2.jpg"
+        alt="..."
+        className="h-[250px] w-96 object-cover rounded-md"
+      />
 
-        <Avatar
-          url="https://www.prensalatina.com.br/wp-content/uploads/2022/01/angola-Luanda.jpg"
-          alt="..."
-          className="h-[250px] w-96 object-cover rounded-md"
-        />
+      <Avatar
+        url="https://www.prensalatina.com.br/wp-content/uploads/2022/01/angola-Luanda.jpg"
+        alt="..."
+        className="h-[250px] w-96 object-cover rounded-md"
+      />
 
-        <Avatar
-          url="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjZ_hxY7Rz0NBzMh2sKbnQ-vwgGZg4NYECq9hQPyzwZVUCRf36hAf8vztgQybWBe6kLblAzSiYGxkYRxeYtwEXly8UgyCuY9hceMFMoDArNmzXHd3PFVcva8Uo9ihoilL8IrcCQC_Nwkv8/s1600/IMG_6899.JPG"
-          alt="..."
-          className="h-[250px] w-96 object-cover rounded-md"
-        />
+      <Avatar
+        url="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjZ_hxY7Rz0NBzMh2sKbnQ-vwgGZg4NYECq9hQPyzwZVUCRf36hAf8vztgQybWBe6kLblAzSiYGxkYRxeYtwEXly8UgyCuY9hceMFMoDArNmzXHd3PFVcva8Uo9ihoilL8IrcCQC_Nwkv8/s1600/IMG_6899.JPG"
+        alt="..."
+        className="h-[250px] w-96 object-cover rounded-md"
+      />
 
-        <Avatar
-          url="https://nadvogados.com/wp-content/uploads/2015/03/luanda.jpg"
-          alt="..."
-          className="h-[250px] w-96 object-cover rounded-md"
-        />
+      <Avatar
+        url="https://nadvogados.com/wp-content/uploads/2015/03/luanda.jpg"
+        alt="..."
+        className="h-[250px] w-96 object-cover rounded-md"
+      />
 
-        <Avatar
-          url="https://s4.static.brasilescola.uol.com.br/img/2013/08/rio-de-janeiro.jpg"
-          alt="..."
-          className="h-[250px] w-96 object-cover rounded-md"
-        />
+      <Avatar
+        url="https://s4.static.brasilescola.uol.com.br/img/2013/08/rio-de-janeiro.jpg"
+        alt="..."
+        className="h-[250px] w-96 object-cover rounded-md"
+      />
 
-        <Avatar
-          url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZz_0pfLMdECfWeoOcQKzkxUNKeDgtHUTSbQ&s"
-          alt="..."
-          className="h-[250px] w-96 object-cover rounded-md"
-        />
+      <Avatar
+        url="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZz_0pfLMdECfWeoOcQKzkxUNKeDgtHUTSbQ&s"
+        alt="..."
+        className="h-[250px] w-96 object-cover rounded-md"
+      />
 
-        <Avatar
-          url="https://blog.luz.vc/wp-content/uploads/2013/05/Rio-de-Janeiro.jpg"
-          alt="..."
-          className="h-[250px] w-96 object-cover rounded-md"
-        />
+      <Avatar
+        url="https://blog.luz.vc/wp-content/uploads/2013/05/Rio-de-Janeiro.jpg"
+        alt="..."
+        className="h-[250px] w-96 object-cover rounded-md"
+      />
 
-        <Avatar
-          url="https://www.meionews.com/uploads/imagens/2023/1/12/webp/vale-uma-visita-cidade-brasileira-e-eleita-uma-das-mais-bonitas-do-mundo-e9001c21-ea5d-4482-9237-cf94ab4864a7.jpg.webp"
-          alt="..."
-          className="h-[250px] w-96 object-cover rounded-md"
-        />
-      </div>
+      <Avatar
+        url="https://www.meionews.com/uploads/imagens/2023/1/12/webp/vale-uma-visita-cidade-brasileira-e-eleita-uma-das-mais-bonitas-do-mundo-e9001c21-ea5d-4482-9237-cf94ab4864a7.jpg.webp"
+        alt="..."
+        className="h-[250px] w-96 object-cover rounded-md"
+      />
     </div>
   )
 }
